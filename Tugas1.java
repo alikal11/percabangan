479213a43279df27301a160bb2986941871e54fe
alikal11 <56871581+alikal11@users.noreply.github.com>
2019-10-22 19:09:52 +0700

    Add files via upload

---
+++ Tugas1.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas1;
+
+/**
+ *
+ * @author asus
+ */
+public class Tugas1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // data ktp
+        long nik = 3514162708960007L;
+        System.out.println(nik);
+        long nim = 201969040052L;
+        System.out.println(nim);
+                
+       
+        String nama = "Alaikal fatoni";
+        System.out.println(nama);
+        String tempatlahir = "pasuruan";
+        System.out.println(tempatlahir);
+        long tanggal = 27081996L;
+        System.out.println(tanggal);
+        String jeniskelamin = "laki-laki";
+        System.out.println(jeniskelamin);
+        String alamat = "sekarputih";
+        System.out.println(alamat);
+        String kelurahan = "mulyorejo";
+        System.out.println(kelurahan);
+        String kecamatan = "kraton";
+        System.out.println(kecamatan);
+        String agama = "islam";
+        System.out.println(agama);
+        String status = "belum menikah";
+        System.out.println(status);
+        String pekerjaan = "karyawan";
+        System.out.println(pekerjaan);
+        String kewarganegaraan = "WNI";
+        System.out.println(kewarganegaraan);
+        String berlaku = "seumur hidup";
+        System.out.println(berlaku);
+        String prodi = "teknik infotmatika";
+        System.out.println(prodi);
+        long angkatan = 2019L;
+        System.out.println(angkatan);}
+    
+    
+}
